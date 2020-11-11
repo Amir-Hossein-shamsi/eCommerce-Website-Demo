765b1608764b14a683da3dd9727374393cde8210
AmirHossein <shamsiamirhossein1@gmail.com>
2020-11-11 12:38:58 -0500

    Setup Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-  <Header />
-    <main>
-      <h1>hi there </h1>  
-   </main>
-  <Footer/>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>hi there</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
